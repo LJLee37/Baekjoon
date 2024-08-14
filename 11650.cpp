56a21c43ffabded2dc97c7746307e205963813b6
LJLee37 <ljlee3759@gmail.com>
2024-08-14 15:46:14 +0900

    Add 11650.cpp

---
+++ 11650.cpp
@@ -0,0 +1,21 @@
+#include <utility>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const auto compare = [](const pair<int, int> &a, const pair<int, int> &b)
+{
+  return a.first == b.first ? a.second < b.second : a.first < b.first;
+};
+int main()
+{
+  int n;
+  cin >> n;
+  vector<pair<int, int>> coordinates(n);
+  for (int i = 0; i < n; i++)
+    cin >> coordinates[i].first >> coordinates[i].second;
+  sort(coordinates.begin(), coordinates.end(), compare);
+  for (const auto &coordinate : coordinates)
+    cout << coordinate.first << ' ' << coordinate.second << '\n';
+  return 0;
+}
